3fd13ac3a0c1d53de029b81c80318ab5af78d19e
sakindia123 <sa6915@gmail.com>
2012-08-20 16:15:58 +0530

    recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,13 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
+#Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/htc/pico/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS           := ../../../device/htc/pico/recovery/graphics.c
+BOARD_RECOVERY_HANDLES_MOUNT := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/pico/prebuilt/recovery_kernel
+TARGET_RECOVERY_INITRC := device/htc/pico/files/recovery.rc
+
 # Yes we do,but let's hash it out 
 # ARCH_ARM_HAVE_VFP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
