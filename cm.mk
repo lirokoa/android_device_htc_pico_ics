ac00904131d48a129f5f3eb2316dab5a5feb6cc6
sakindia123 <sa6915@gmail.com>
2012-08-24 14:57:43 +0530

    cm.mk Cleanups

--- cm.mk
+++ cm.mk
@@ -1,26 +1,13 @@
 # Prepare for new BootAnimation
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
-# Include GSM stuff
+# Include configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit from those products. Most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
-# Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
-
-# Inherit device configuration
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, device/htc/pico/pico.mk)
-
-# Include FM-Radio stuff
-#$(call inherit-product, vendor/cm/products/bcm_fm_radio.mk)
 
 PRODUCT_NAME := cm_pico
 PRODUCT_BRAND := htc_europe
