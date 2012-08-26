9ab992b88ca6ed6ef72ca0769dba669b039b2d57
lirokoa <lirokoa@hotmail.com>
2012-08-26 21:58:55 +0200

    cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +15,6 @@
 
 USE_CAMERA_STUB := true
 
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-
-# Needs Review
-#BOARD_CAMERA_USE_GETBUFFERINFO := true
-# This is needed by libcamera.so
-BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
-BOARD_USE_CAF_LIBCAMERA := true
-
 # Arch related defines
 TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
@@ -31,8 +23,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-# Yes we do,but let's hash it out 
-# ARCH_ARM_HAVE_VFP := true
+# Yes we do,but let's hash it out
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
@@ -40,7 +31,6 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := pico
 BOARD_NO_SPEAKER := true # msm7627a doesn't have speaker
 TARGET_CORTEX_CACHE_LINE_32 := true
-TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
@@ -70,7 +60,7 @@ BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 
-#  Bluetooth
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
@@ -82,18 +72,17 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USES_QCOM_LIBS := true
 TARGET_GRALLOC_USES_ASHMEM := true
-COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
 
 # Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-BOARD_WLAN_DEVICE                := bcm4330
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4330.ko"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4330b2.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4330b2_apsta.bin"
-WIFI_DRIVER_MODULE_NAME          := "bcm4330"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/firmware/fw_bcm4330b2.bin nvram_path=/proc/calibration iface_name=eth0"
+BOARD_WLAN_DEVICE := bcm4330
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4330.ko"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcm4330b2.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/fw_bcm4330b2_apsta.bin"
+WIFI_DRIVER_MODULE_NAME := "bcm4330"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4330b2.bin nvram_path=/proc/calibration iface_name=eth0"
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/pico/releasetools
@@ -104,18 +93,19 @@ BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := pico
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-#TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-DCHECK_FOR_EXTERNAL_FORMAT := true
+
+# Graphics
 BOARD_USE_SKIA_LCDTEXT := true
-BOARD_FORCE_DITHERING := true
-USE_OPENGL_RENDERER := true
 TARGET_USES_GENLOCK := true
+USE_OPENGL_RENDERER := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 TARGET_FORCE_CPU_UPLOAD := true
-BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
-#COMMON_GLOBAL_CFLAGS += -DMISSING_GRALOC_BUFFERS 
-#COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_PIXEL_FORMAT_YV12 
+
+#BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+#COMMON_GLOBAL_CFLAGS += -DMISSING_GRALOC_BUFFERS
+#COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_PIXEL_FORMAT_YV12
 #COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE
-COMMON_GLOBAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
+#COMMON_GLOBAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
 
 # Browser
 WITH_JIT := true
@@ -123,7 +113,7 @@ ENABLE_JSC_JIT := true
 JS_ENGINE := v8
 HTTP := chrome
 
-# ICS Stuff 
+# ICS Stuff
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # RIL
@@ -135,7 +125,7 @@ WITH_JIT := true
 ENABLE_JSC_JIT := true
 JS_ENGINE := v8
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=65
 
 # Touch screen compatibility for ICS
 BOARD_USE_LEGACY_TOUCHSCREEN := true
