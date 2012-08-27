2cfd30f588007bf5671de84a8c0cc21a548cd144
sakindia123 <sa6915@gmail.com>
2012-08-27 15:58:45 +0530

    Fixes from lirokoa,allow libaudio to build

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(my-dir)
 
 ifeq ($(TARGET_DEVICE),pico)
     subdir_makefiles := \
-#disable audio for now(compile errors)	 $(LOCAL_PATH)/libaudio/Android.mk \
+         $(LOCAL_PATH)/libaudio/Android.mk \
 #	 $(LOCAL_PATH)/libacoustic/Android.mk \
 #	 $(LOCAL_PATH)/libcamera/Android.mk \
      include $(subdir_makefiles)
