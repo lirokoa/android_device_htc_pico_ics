91ce685e027fdd133424092de38a93f62ac3e09e
sakindia123 <sa6915@gmail.com>
2012-08-27 18:24:06 +0530

    Derp

--- releasetools/extras.sh
+++ releasetools/extras.sh
@@ -8,7 +8,7 @@ sys_size=$(df -h system|grep dev/block|awk '{ print substr($2,1,length($2)-3) }'
 # Make sure setprop link exists for older CWM versions.
 ln -s /sbin/recovery /sbin/setprop
 
-if [ $sys_size -ge 210 ]; then
+if [ $sys_size -ge 243 ]; then
     setprop install.extras 1
 else
     setprop install.extras 0
