304e075056ce2bc55c1c0dfc59dfca8a3aac075f
sakindia123 <sa6915@gmail.com>
2012-09-27 11:15:08 +0530

    Add "turbo" cached memory cleaner

--- pico.mk
+++ pico.mk
@@ -111,6 +111,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/pico/prebuilt/bcmdhd.ko:system/lib/modules/bcmdhd.ko \
     device/htc/pico/prebuilt/kineto_gan.ko:system/lib/modules/kineto_gan.ko \
+    device/htc/pico/prebuilt/turbo:system/bin 
 
 # Wifi
 PRODUCT_COPY_FILES += \
