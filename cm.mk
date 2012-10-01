67d0a746a058a0755e523b5be7e081538ce75d8a
lirokoa <lirokoa@hotmail.com>
2012-10-01 18:05:36 +0200

    Typo

--- cm.mk
+++ cm.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/htc/pico/pico.mk)
 PRODUCT_NAME := cm_pico
 PRODUCT_BRAND := htc_europe
 PRODUCT_DEVICE := pico
-PRODUCT_MODEL := HTC Explorer A310
+PRODUCT_MODEL := HTC Explorer A310e
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=IMLK74 BUILD_FINGERPRINT=htc_asia_india/htc_pico/pico:2.3.5/GRJ90/171430.1:user/release-keys PRIVATE_BUILD_DESC="1.12.720.1 CL171430 release-keys"
 
