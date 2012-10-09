c123afe271c55805a51a856abe75929d3fdc799a
lirokoa <lirokoa@hotmail.com>
2012-10-09 19:21:47 +0200

    Clean up and compile CM9 with full base ! (reverse-merged from commit 14e2c8b186f23b536a97a6c6dbe511dc4ac58f55)

--- cm.mk
+++ cm.mk
@@ -1,21 +1,24 @@
 # Prepare for new BootAnimation
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
-# Include GSM stuff
+# Inherit from those products. Most specific first.
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/pico/pico.mk)
 
-
-# Release name
-PRODUCT_RELEASE_NAME := pico
-
 PRODUCT_NAME := cm_pico
-PRODUCT_BRAND := HTC
+PRODUCT_BRAND := htc_europe
 PRODUCT_DEVICE := pico
 PRODUCT_MODEL := HTC Explorer A310e
 PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=IMM76L BUILD_FINGERPRINT=htc_asia_india/htc_pico/pico:2.3.5/GRJ90/171430.1:user/release-keys PRIVATE_BUILD_DESC="1.12.720.1 CL171430 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := pico
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+ 
