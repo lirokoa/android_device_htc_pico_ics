012ab06f1f875ff8456e668c24ee6e76ff93f37c
sakindia123 <sa6915@gmail.com>
2012-10-31 16:56:05 +0530

    Forgot to remove old kernel modules

--- pico.mk
+++ pico.mk
@@ -130,12 +130,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/pico/files/etc/vold.fstab:system/etc/vold.fstab 
 
-# Prebuilt Modules
-PRODUCT_COPY_FILES += \
-    device/htc/pico/prebuilt/bcmdhd.ko:system/lib/modules/bcmdhd.ko \
-    device/htc/pico/prebuilt/kineto_gan.ko:system/lib/modules/kineto_gan.ko \
-    device/htc/pico/prebuilt/tun.ko:system/lib/modules/tun.ko \
-    
 # Wifi
 PRODUCT_COPY_FILES += \
     device/htc/pico/files/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
